5094f8c6dc7d2594fe218df3b97d34195666690d
lemelinha <lucas.leme.0806@gmail.com>
2024-04-30 20:29:38 -0300

    inicio do home

--- App/Views/index.php
+++ App/Views/index.php
@@ -1,1 +1,40 @@
-estou no IndexController
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buffet</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body id="home">
+    <header>
+        <img src="" alt="logo">
+        <nav>
+            <a href="#sobre-projeto">Sobre o Projeto</a>
+            <a href="#detalhes">Detalhes</a>
+            <a href="#sobre-nos">Sobre nós</a>
+            <a href="#">Contato</a>
+            <a href="/login">Já sou cliente</a>
+        </nav>
+    </header>
+    <main>
+        <section id="sobre-projeto">
+            <!-- Imagem de fundo (slide show) -->
+            <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci nisi sunt rerum nemo vitae voluptas culpa, corrupti beatae quasi aspernatur! Animi nulla modi doloremque, aliquid quae porro quibusdam consequatur fuga?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam amet, itaque, aut ab eius provident consequatur quam similique magnam, labore dicta ipsum optio accusamus laborum. Odit qui id inventore velit?
+            </div>
+        </section>
+        <section id="detalhes">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum, optio. Tempore ex facere explicabo iure magnam cupiditate dicta maiores consequuntur reprehenderit. Sapiente, sunt expedita optio magni odit saepe voluptatem nobis?Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque cum nesciunt deserunt sint ullam est similique vitae quidem necessitatibus excepturi earum quis, libero quia cupiditate iure atque, possimus magni id. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus itaque facilis, repellendus odit voluptatum maxime asperiores commodi magnam quod! Qui debitis nesciunt deserunt, illo quis ducimus quod? Recusandae, non explicabo? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, delectus? Sint, repellendus. Corrupti, possimus inventore blanditiis adipisci mollitia sit aperiam, voluptas ipsam quae vitae error accusamus nemo cupiditate, temporibus exercitationem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur amet illum, ad ipsam odio vero. Maiores, adipisci. Deserunt nostrum cumque, perferendis blanditiis maxime officiis! Eligendi enim deleniti corrupti sequi accusamus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quia aliquam at in, eius cumque laudantium repudiandae nihil magnam asperiores maxime quod minima atque laborum nostrum tempora. Placeat, atque totam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic id, voluptatem excepturi nisi assumenda porro omnis ea expedita, eos vel reprehenderit consectetur fuga provident, iusto facere incidunt error eligendi facilis.
+        </section>
+        <section id="sobre-nos">
+            <!-- Breve apresentação da gente -->
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur nam dicta quaerat id maxime deserunt odio qui eaque. Voluptatibus libero nostrum error quia et deleniti, nam iste culpa ea pariatur.
+        </section>
+    </main>
+    <footer>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. At sequi distinctio impedit fugiat vitae explicabo qui magni sunt sed praesentium, rerum, consequuntur quaerat minus harum cum nisi. Velit, iusto soluta!
+    </footer>
+    <script src="assets/js/index.js"></script>
+</body>
+</html>
