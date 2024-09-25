b301215d7fe780f92d10aaec26364813f119adc0
Leo-Slv <leonardo1692004@gmail.com>
2024-09-25 16:53:47 -0300

    Titulo e cards no main

--- main.php
+++ main.php
@@ -1,5 +1,128 @@
 <main>
-    <figure>
-    
-    </figure>
+    <header class="text-center font-bold p-4">
+     <h1 class="text-xl md:text-4xl lg:text-6xl">Produtos Cadastrados</h1>
+    </header>
+    <div class="grid grid-cols-1 p-4 gap-12 md:grid-cols-3 md:gap-16 lg:grid-cols-4 lg:gap-12">
+        <div class="rounded-lg bg-theme border-4     border-amber-300 p-4">
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                </div>
+        <div class="rounded-lg bg-theme border-4     border-amber-300 p-4">
+            <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+        </div>
+        <div class="rounded-lg bg-theme border-4     border-amber-300 p-4">
+            <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+        </div>
+        <div class="rounded-lg bg-theme border-4     border-amber-300 p-4">
+            <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+        </div>
+        <div class="rounded-lg bg-theme border-4     border-amber-300 p-4">
+            <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+        </div>
+        <div class="rounded-lg bg-theme border-4     border-amber-300 p-4">
+            <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+        </div>
+        <div class="rounded-lg bg-theme border-4     border-amber-300 p-4">
+            <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+        </div>
+        <div class="rounded-lg bg-theme border-4     border-amber-300 p-4">
+            <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+                <a href="." class="text-white font-bold">Protudos Cadastrados</a>
+        </div>
+    </div>
 </main>
+
