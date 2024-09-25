784eec4919959cce460a7eccb8cb93af978745e0
Leo-Slv <leonardo1692004@gmail.com>
2024-09-24 21:46:11 -0300

    front

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'header.php';
+
+
+
+?>
+
+<body class='flex row'>
+
+<?php
+
+require_once 'side-bar.php';
+
+require_once 'main.php';
+
+require_once 'footer.php';
+
+?>
+</body>
