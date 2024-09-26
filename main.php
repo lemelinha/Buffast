9806c319d7f195c5080773454f8c6c5a2c6668b8
Leo-Slv <leonardo1692004@gmail.com>
2024-09-26 16:17:29 -0300

    Implementação de font teste no main

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 <main>
     <header class="text-center main-font text-white font-bold">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" fill="#803469"><path d="M0 0v100c166.7 0 166.7-66 333.3-66S500 77 666.7 77 833.3 28 1000 28V0H0Z" opacity=".5"></path><path d="M0 0v100c166.7 0 166.7-66 333.3-66S500 70 666.7 70 833.3 16 1000 16V0H0Z" opacity=".5"></path><path d="M0 0v100c166.7 0 166.7-66 333.3-66S500 63 666.7 63 833.3 4 1000 4V0H0Z"></path></svg>
-     <h1 class="text-xl font-tittle md:text-4xl lg:text-6xl">Produtos Cadastrados</h1>
+     <h1 class="text-3xl font-tittle md:text-4xl lg:text-6xl">Produtos Cadastrados</h1>
     </header>
     <div class="grid grid-cols-1 p-2 gap-12 md:grid-cols-3 md:gap-16 lg:grid-cols-4 p-8 lg:gap-12">
         <div class="bg-card rounded-lg shadow-2xl p-4">
