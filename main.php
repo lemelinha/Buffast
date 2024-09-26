63dc6d4eae77114cf7f49be365a18e5ecd1f302a
Leo-Slv <leonardo1692004@gmail.com>
2024-09-26 16:33:18 -0300

    Inserção de nova imagem no main

--- main.php
+++ main.php
@@ -5,7 +5,7 @@
     </header>
     <div class="grid grid-cols-1 p-2 gap-12 md:grid-cols-3 md:gap-16 lg:grid-cols-4 p-8 lg:gap-12">
         <div class="bg-card rounded-lg shadow-2xl p-4">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoDeR4BcH8f1klSkGe46EDlwMn3AiJrs_vnw&s" class="rounded-lg shadow-2xl pb-2">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoDeR4BcH8f1klSkGe46EDlwMn3AiJrs_vnw&s" class="rounded-lg shadow-2xl">
                 <section class="grid grid-cols-1">
                     <a href="." class="text-white font-bold">Nome do Produto: Coxinha</a>
                     <a href="." class="text-white font-bold">Quantidade no Pote: 5</a>
@@ -13,7 +13,7 @@
                 </section>
         </div>
         <div class="bg-card rounded-lg shadow-2xl p-4">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoDeR4BcH8f1klSkGe46EDlwMn3AiJrs_vnw&s" class="rounded-lg shadow-2xl">
+                <img src="./assets/queijo.jpg" class="rounded-lg shadow-2xl">
                  <section class="grid grid-cols-1">
                     <a href="." class="text-white font-bold">Nome do Produto: Coxinha</a>
                     <a href="." class="text-white font-bold">Quantidade no Pote: 5</a>
