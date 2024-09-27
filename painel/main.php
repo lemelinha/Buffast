412b46ed08b0119f95a477a3dcae31fd0bcd4541
Leo-Slv <leonardo1692004@gmail.com>
2024-09-27 03:35:47 -0300

    Insereção da mascara do filtro e cards adicionais

---
+++ painel/main.php
@@ -0,0 +1,109 @@
+<main class="ml-16">
+    <header class="text-center main-font text-white font-bold">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" fill="#803469"><path d="M0 0v100c166.7 0 166.7-66 333.3-66S500 77 666.7 77 833.3 28 1000 28V0H0Z" opacity=".5"></path><path d="M0 0v100c166.7 0 166.7-66 333.3-66S500 70 666.7 70 833.3 16 1000 16V0H0Z" opacity=".5"></path><path d="M0 0v100c166.7 0 166.7-66 333.3-66S500 63 666.7 63 833.3 4 1000 4V0H0Z"></path></svg>
+     <h1 class="text-3xl font-tittle md:text-4xl lg:text-6xl">Produtos Cadastrados</h1>
+    </header>
+    <aside class="pt-8 flex justify-end justify-items-center">
+        <label class=" font-tittle main-font text-lg ml-4 md:text-2xl lg:text-3xl">Filtro</label>
+        <input type="text" class="bg-theme text-white font-bold rounded-full p-2 ml-4 mr-8 w-40 h-8 md:w-52 md:h-8 lg:w-52 lg:h-8 "></input>
+    </aside>
+    <div class="grid grid-cols-1 md:text-sm lg:text-base gap-12 md:grid-cols-3 md:gap-16 lg:grid-cols-4 p-8 lg:gap-12">
+        <div class="bg-card rounded-lg shadow-2xl p-4">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoDeR4BcH8f1klSkGe46EDlwMn3AiJrs_vnw&s" class="rounded-lg shadow-2xl">
+                <section class="grid grid-cols-1 text-white font-bold p-2 text-xs md:text-sm lg:text-base">
+                    <a href=".">Nome do Produto: Coxinha</a>
+                    <a href=".">Quantidade no Pote: 5</a>
+                    <a href=".">Descrição do Produto: Coxinha de Frango</a>
+                </section>
+        </div>
+        <div class="bg-card rounded-lg shadow-2xl p-4">
+                <img src="../assets/queijo.jpg" class="rounded-lg shadow-2xl">
+                 <section class="grid grid-cols-1 text-white font-bold p-2 text-xs md:text-sm lg:text-base">
+                    <a href=".">Nome do Produto: Bolinha</a>
+                    <a href=".">Quantidade no Pote: 5</a>
+                    <a href=".">Descrição do Produto: Bolinha de Queijo</a>
+                </section>
+        </div>
+        <div class="bg-card rounded-lg shadow-2xl p-4">
+                <img src="../assets/pastel.jpg" class="rounded-lg shadow-2xl">
+                 <section class="grid grid-cols-1 text-white font-bold p-2 text-xs md:text-sm lg:text-base">
+                    <a href=".">Nome do Produto: Pastel</a>
+                    <a href=".">Quantidade no Pote: 3</a>
+                    <a href=".">Descrição do Produto: Pastel de Frango</a>
+                </section>
+        </div>
+        <div class="bg-card rounded-lg shadow-2xl p-4">
+                <img src="../assets/pastel.jpg" class="rounded-lg shadow-2xl bg-cover">
+                 <section class="grid grid-cols-1 text-white font-bold p-2 text-xs md:text-sm lg:text-base">
+                    <a href=".">Nome do Produto: Pastel</a>
+                    <a href=".">Quantidade no Pote: 3</a>
+                    <a href=".">Descrição do Produto: Pastel de Carne</a>
+                </section>
+        </div>
+        <div class="bg-card rounded-lg shadow-2xl p-4">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoDeR4BcH8f1klSkGe46EDlwMn3AiJrs_vnw&s" class="rounded-lg shadow-2xl">
+                <section class="grid grid-cols-1 text-white font-bold p-2 text-xs md:text-sm lg:text-base">
+                    <a href=".">Nome do Produto: Coxinha</a>
+                    <a href=".">Quantidade no Pote: 5</a>
+                    <a href=".">Descrição do Produto: Coxinha de Frango</a>
+                </section>
+        </div>
+        <div class="bg-card rounded-lg shadow-2xl p-4">
+                <img src="../assets/queijo.jpg" class="rounded-lg shadow-2xl">
+                 <section class="grid grid-cols-1 text-white font-bold p-2 text-xs md:text-sm lg:text-base">
+                    <a href=".">Nome do Produto: Bolinha</a>
+                    <a href=".">Quantidade no Pote: 5</a>
+                    <a href=".">Descrição do Produto: Bolinha de Queijo</a>
+                </section>
+        </div>
+        <div class="bg-card rounded-lg shadow-2xl p-4">
+                <img src="../assets/pastel.jpg" class="rounded-lg shadow-2xl">
+                 <section class="grid grid-cols-1 text-white font-bold p-2 text-xs md:text-sm lg:text-base">
+                    <a href=".">Nome do Produto: Pastel</a>
+                    <a href=".">Quantidade no Pote: 3</a>
+                    <a href=".">Descrição do Produto: Pastel de Frango</a>
+                </section>
+        </div>
+        <div class="bg-card rounded-lg shadow-2xl p-4">
+                <img src="../assets/pastel.jpg" class="rounded-lg shadow-2xl bg-cover">
+                 <section class="grid grid-cols-1 text-white font-bold p-2 text-xs md:text-sm lg:text-base">
+                    <a href=".">Nome do Produto: Pastel</a>
+                    <a href=".">Quantidade no Pote: 3</a>
+                    <a href=".">Descrição do Produto: Pastel de Carne</a>
+                </section>
+        </div>
+        <div class="bg-card rounded-lg shadow-2xl p-4">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoDeR4BcH8f1klSkGe46EDlwMn3AiJrs_vnw&s" class="rounded-lg shadow-2xl">
+                <section class="grid grid-cols-1 text-white font-bold p-2 text-xs md:text-sm lg:text-base">
+                    <a href=".">Nome do Produto: Coxinha</a>
+                    <a href=".">Quantidade no Pote: 5</a>
+                    <a href=".">Descrição do Produto: Coxinha de Frango</a>
+                </section>
+        </div>
+        <div class="bg-card rounded-lg shadow-2xl p-4">
+                <img src="../assets/queijo.jpg" class="rounded-lg shadow-2xl">
+                 <section class="grid grid-cols-1 text-white font-bold p-2 text-xs md:text-sm lg:text-base">
+                    <a href=".">Nome do Produto: Bolinha</a>
+                    <a href=".">Quantidade no Pote: 5</a>
+                    <a href=".">Descrição do Produto: Bolinha de Queijo</a>
+                </section>
+        </div>
+        <div class="bg-card rounded-lg shadow-2xl p-4">
+                <img src="../assets/pastel.jpg" class="rounded-lg shadow-2xl">
+                 <section class="grid grid-cols-1 text-white font-bold p-2 text-xs md:text-sm lg:text-base">
+                    <a href=".">Nome do Produto: Pastel</a>
+                    <a href=".">Quantidade no Pote: 3</a>
+                    <a href=".">Descrição do Produto: Pastel de Frango</a>
+                </section>
+        </div>
+        <div class="bg-card rounded-lg shadow-2xl p-4">
+                <img src="../assets/pastel.jpg" class="rounded-lg shadow-2xl bg-cover">
+                 <section class="grid grid-cols-1 text-white font-bold p-2 text-xs md:text-sm lg:text-base">
+                    <a href=".">Nome do Produto: Pastel</a>
+                    <a href=".">Quantidade no Pote: 3</a>
+                    <a href=".">Descrição do Produto: Pastel de Carne</a>
+                </section>
+        </div>
+    </div>
+</main>
+
