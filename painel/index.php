cafc138083d89a4176623cf3fc59ef8ca7ae0cc7
Leo-Slv <leonardo1692004@gmail.com>
2024-09-27 03:34:55 -0300

    Criação da pasta painel e adição de arquivos

---
+++ painel/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'header.php';
+
+
+
+?>
+
+<body class='flex'>
+
+<?php
+
+require_once 'side-bar.php';
+
+require_once 'main.php';
+
+require_once 'footer.php';
+
+?>
+</body>
