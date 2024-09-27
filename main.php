2a675def027e427930c5342cae8b670499248a65
Leo-Slv <leonardo1692004@gmail.com>
2024-09-26 21:23:27 -0300

    Inserção de imagem do pastel de carne

--- main.php
+++ main.php
@@ -1,4 +1,4 @@
-<main>
+<main class="ml-16">
     <header class="text-center main-font text-white font-bold">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" fill="#803469"><path d="M0 0v100c166.7 0 166.7-66 333.3-66S500 77 666.7 77 833.3 28 1000 28V0H0Z" opacity=".5"></path><path d="M0 0v100c166.7 0 166.7-66 333.3-66S500 70 666.7 70 833.3 16 1000 16V0H0Z" opacity=".5"></path><path d="M0 0v100c166.7 0 166.7-66 333.3-66S500 63 666.7 63 833.3 4 1000 4V0H0Z"></path></svg>
      <h1 class="text-3xl font-tittle md:text-4xl lg:text-6xl">Produtos Cadastrados</h1>
@@ -21,7 +21,7 @@
                 </section>
         </div>
         <div class="bg-card rounded-lg shadow-2xl p-4">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoDeR4BcH8f1klSkGe46EDlwMn3AiJrs_vnw&s" class="rounded-lg shadow-2xl">
+                <img src="./assets/pastel_carne.jpg" class="rounded-lg shadow-2xl">
                  <section class="grid grid-cols-1 p-2">
                     <a href="." class="text-white font-bold">Nome do Produto: Coxinha</a>
                     <a href="." class="text-white font-bold">Quantidade no Pote: 5</a>
