89667895a2d573ed22912a8f439ea5506351820d
Leo-Slv <leonardo1692004@gmail.com>
2024-09-27 22:18:20 -0300

    Tela de cadastrar com mascara de botão

---
+++ painel/cadastrar.php
@@ -0,0 +1,25 @@
+<?php require_once "header.php";?>
+
+<?php require_once "side-bar.php";?>
+
+<main class="ml-16">
+    <header class="text-center main-font text-white font-bold">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" fill="#803469"><path d="M0 0v100c166.7 0 166.7-66 333.3-66S500 77 666.7 77 833.3 28 1000 28V0H0Z" opacity=".5"></path><path d="M0 0v100c166.7 0 166.7-66 333.3-66S500 70 666.7 70 833.3 16 1000 16V0H0Z" opacity=".5"></path><path d="M0 0v100c166.7 0 166.7-66 333.3-66S500 63 666.7 63 833.3 4 1000 4V0H0Z"></path></svg>
+     <h1 class="text-3xl font-tittle p-8 md:text-4xl lg:text-6xl lg:p-0">Cadastrar</h1>
+    </header>
+    <div class="flex justify-center pt-96  justify-items-center p-8 font-bold lg:pr-24 lg:pl-24 lg:pt-4 lg:min-h-3.5">
+        <h1 href="" class="text-3xl h-16 p-4 rounded-full bg-btn main-font text-amber-300 mt-56 md:text-4xl  lg:text-6xl lg:p-2">+Produtos</h1>
+    </div>
+</main>
+
+<!-- <div class="grid h-screen p-8 grid-cols-1 lg:pr-24 lg:pl-24 pt-4 lg:min-h-3.5">
+        <div class="bg-card-cad rounded-lg shadow-2xl p-4">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoDeR4BcH8f1klSkGe46EDlwMn3AiJrs_vnw&s" class="rounded-lg shadow-2xl">
+                <section class="grid grid-cols-1 text-white font-bold p-2 text-xs md:text-sm lg:text-sm">
+                    <a href=".">Nome do Produto: Coxinha</a>
+                    <a href=".">Quantidade no Pote: 5</a>
+                    <a href=".">Descrição do Produto: Coxinha de Frango</a>
+                </section>
+        </div>
+    </div>
+    
