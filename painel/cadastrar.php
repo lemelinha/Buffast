f8227298820e23e8badd7a548baf4511f7c6f502
Leo-Slv <leonardo1692004@gmail.com>
2024-10-16 22:23:10 -0300

    Centralizado o botão inferior com grid

--- painel/cadastrar.php
+++ painel/cadastrar.php
@@ -2,13 +2,13 @@
 
 <?php require_once "side-bar.php";?>
 
-<main class="ml-16">
+<main class="grid grid-rows-8 ml-16 h-screen">
     <header class="text-center main-font text-white font-bold">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" fill="#803469"><path d="M0 0v100c166.7 0 166.7-66 333.3-66S500 77 666.7 77 833.3 28 1000 28V0H0Z" opacity=".5"></path><path d="M0 0v100c166.7 0 166.7-66 333.3-66S500 70 666.7 70 833.3 16 1000 16V0H0Z" opacity=".5"></path><path d="M0 0v100c166.7 0 166.7-66 333.3-66S500 63 666.7 63 833.3 4 1000 4V0H0Z"></path></svg>
-     <h1 class="text-4xl font-tittle p-8 md:text-5xl lg:text-6xl lg:p-0">Cadastrar</h1>
+     <h1 class="text-4xl font-tittle p-8 md:text-5xl lg:text-6xl lg:p-0">Festas</h1>
     </header>
-    <div class=" flex justify-center justify-items-center mt-96 p-8 font-bold md:mt-96 md:p-8 lg:mt-80 lg:p-8 lg:min-h-3.5">
-        <button href="" class="bg-btn text-4xl h-16 p-2 rounded-full main-font text-amber-300 md:text-5xl lg:text-6xl lg:p-2">+Produtos</button>
+    <div class=" grid grid-rows-subgrid row-span-8 justify-center justify-items-center p-8 font-bold md: md:p-8 lg: lg:p-8 lg:min-h-3.5">
+        <button href="" class="row-start-8 bg-btn text-4xl h-16 p-2 rounded-full main-font text-amber-300 md:text-5xl lg:text-6xl lg:p-2">+Cadastrar</button>
     </div>
 </main>
 
