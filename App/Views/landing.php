7bd291a358392beb3b022534229c2f320a093eeb
Leo-Slv <leonardo1692004@gmail.com>
2024-11-18 17:37:33 -0300

    Continuação de desing da landing page

--- App/Views/landing.php
+++ App/Views/landing.php
@@ -9,14 +9,19 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css"  rel="stylesheet" />
 </head>
-<body>
+<body class="bg-landing">
+  <nav class="grid grid-cols-2 p-3 bg-amber-300">
+      <h6 class="mt-2 main-font font-tittle text-3xl">Buffast</h6>
+    <aside class="main-font justify-self-end">
+      <button class="p-2 text-base lg:text-lg text-amber-300 border border-gray-300 rounded-lg bg-card-modal focus:ring-blue-500 focus:border-blue-500">Login</button>
+      <button class="p-2 text-base lg:text-lg text-amber-300 border border-gray-300 rounded-lg bg-card-modal focus:ring-blue-500 focus:border-blue-500">Cadastrar</button>
+    </aside>
+  </nav>
   <main>
-    <nav class="grid grid-cols-2 p-4 bg-amber-300">
-        <h6 class="mt-2 main-font font-tittle text-3xl">Buffast</h6>
-      <aside class="main-font justify-self-end">
-        <button class="p-2 text-lg text-amber-300 border border-gray-300 rounded-lg bg-card-modal focus:ring-blue-500 focus:border-blue-500">Login</button>
-        <button class="p-2 text-lg text-amber-300 border border-gray-300 rounded-lg bg-card-modal focus:ring-blue-500 focus:border-blue-500">Cadastrar</button>
-      </aside>
-    </nav>
+    <article class="center text-center main-font ">
+      <p class="text-6xl text-amber-300">Mais Agilidade e Flexibilidade para Seus Eventos!</p>
+      <p class="p-4 text-xl text-amber-400">Pedidos Mais Precisos, Clientes Mais Felizes: Transforme Seu Buffet com Facilidade e Rapidez!</p>
+      <button class="p-2 text-base lg:text-4xl text-amber-300 border border-gray-300 rounded-lg bg-card-modal focus:ring-blue-500 focus:border-blue-500">Comece Agora</button>
+    </article>
   </main>
 </body>
