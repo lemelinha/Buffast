8b52f90d645f924069f2349601cd257fed7b1664
Lucas Leme <67035515+lemelinha@users.noreply.github.com>
2024-11-21 23:33:54 -0300

    Delete buffast_initial_data.sql

--- buffast_initial_data.sql
+++
@@ -1,4 +0,0 @@
-use db_buffast;
-
-insert into tb_buffet values 
-('c6cd4b5f-a4da-4e81-8af0-59af118765c8', 'Nb/2rG9vpO17K6gtfFSVZTo6UMb8m5RUdxAHv/ZhZhaRFA==', '5JuZENmxvAjv2pqd2506HIBsu50FyAlXUnzELmWJKEs6Og6oD4wEtazA2o6l9TxlEbE=', null, '$2y$10$z8AFnHDQt89.GBWQOqijDuhWFOHdr4NEXklkIfPZgJEMWnGODIFHu', '+1a2kMTA2TzBt5C2QJqT0wFfJOQc9W9REsjUaMwWDqM6OspG1zPYuyS/2H0mhSMRAhw=', 'V');
