1a96b5b2021655e31755a938e021870e002c3484
lemelinha <lucas.leme.0806@gmail.com>
2024-11-22 22:10:18 -0300

    teste email 4

--- App/Models/Register.php
+++ App/Models/Register.php
@@ -10,18 +10,18 @@ class Register extends Model {
         $mail->isSMTP();
         $mail->Host = 'smtp.hostinger.com';
         $mail->SMTPAuth = true;
-        $mail->SMTPSecure = 'SSL';
+        $mail->SMTPSecure = 'ssl';
         $mail->Username = 'support@buffast.com.br';
         $mail->Password = 'Beta5000@';
         $mail->Port = 465;
-
-        $mail->setFrom($mail->Username);
-        $mail->addReplyTo('globglogabgalabcapeta666@gmail.com');
+        
+        $mail->setFrom('support@buffast.com.br');
+        $mail->addReplyTo('globglogabgalabcapeta666@gmail.com', 'asd');
         $mail->addAddress('l.leme3008@gmail.com', 'Lucas Leme');
         $mail->addAddress('kauzago9@gmail.com', 'Kauan Zago');
         $mail->addCC('globglogabgalabcapeta666@gmail.com', 'Cópia');
         $mail->addBCC('globglogabgalabcapeta666@gmail.com', 'Cópia Oculta');
-
+        
         $mail->isHTML(true);
         $mail->Subject = 'Autentificar Registro';
         $mail->Body    = 'Autentifique seu registro no Buffast! (aguarde)';
