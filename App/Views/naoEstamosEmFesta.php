b689d7f53989b261fd831192e0916a6f286e95b7
Leo-Slv <leonardo1692004@gmail.com>
2024-12-03 15:45:47 -0300

    tela de nao estar em festa

--- App/Views/naoEstamosEmFesta.php
+++ App/Views/naoEstamosEmFesta.php
@@ -1,1 +1,19 @@
-Desculpe, acho que nao estamos em festa
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buffast</title>
+    <link href="/assets/css/style.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css"  rel="stylesheet" />
+    <link rel="icon" type="image/png" href="/assets/images/Logo-Buffast2.png" class="h-8 w-8">
+</head>
+<body>
+  <div class="center bg-landing h-screen w-screen text-center place-content-center main-font">
+    <p class="text-amber-300 text-5xl ">Desculpe, acho que nao estamos em festa.</p>
+    <a href="/" class="text-white border-b-2 border-white text-3xl">Voltar</a>
+  </div>
+</body>
